58c0904fffccbd772824e4692b33664f041be03d
Ivanilson Santos <nilson.98@hotmail.com>
2022-05-03 02:16:41 -0300

    Projeto Finalizado (falta deixar responsivo)

--- js/index.js
+++ js/index.js
@@ -12,15 +12,35 @@ let combinacoes = [ [1,0,2,2,0],
                     [0,2,0,1,2],
                     [2,0,2,0,1],];
 
-function game(jogador,maquina){
+function game(jogador,maquina,objeto){
 
     let resultado = combinacoes[jogador][maquina]
     if(resultado==0){
         pontuacaoMaquina++
+        objeto.style.borderColor = "red"
+        objeto.style.borderWidth = "5px"
+        personagens[escolhaMaquina].style.borderColor="green"
+        personagens[escolhaMaquina].style.borderWidth="5px"
+        document.querySelector("#mensagem span").innerHTML = "Você Perdeu!"
+        
     }
-    if(resultado==2){
+    else if(resultado==2){
         pontuacaojogador++
+        objeto.style.borderColor = "green"
+        objeto.style.borderWidth = "5px"
+        personagens[escolhaMaquina].style.borderColor="red"
+        personagens[escolhaMaquina].style.borderWidth="5px"
+        document.querySelector("#mensagem span").innerHTML = "Você Ganhou!"
     }
+    else{
+        document.querySelector("#mensagem span").innerHTML = "Empate!"
+        document.getElementById("jogador").innerHTML=""
+        document.getElementById("maquina").innerHTML=""
+    }
+}
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 function generateRandomChar(){
@@ -30,11 +50,12 @@ function generateRandomChar(){
 
 }
 
-function eraseBorders(){
+function resetBorders(){
 
     document.querySelectorAll(".personagens").forEach((curr)=>{
 
         curr.style.borderColor=""
+        curr.style.borderWidth=""
         
     })
 }
@@ -42,14 +63,14 @@ function eraseBorders(){
 document.querySelectorAll(".personagens").forEach((curr,index)=>{
     
     curr.addEventListener("click",(e)=>{
-        
+        resetBorders()
         escolhaMaquina = generateRandomChar()
-        e.target.style.borderColor = "green"
-        personagens[escolhaMaquina].style.borderColor="red"
-        game(index, escolhaMaquina)
-       
-        console.log(personagens[index].id+" "+personagens[escolhaMaquina].id)
-        document.querySelector("#placar span").innerHTML = "Jogador"+String(pontuacaojogador)+"x"+String(pontuacaoMaquina)+"Máquina"
+        let targetObject = e.target.tagName=="IMG"?e.target.parentNode:e.target
+        game(index, escolhaMaquina,targetObject)
+
+        document.getElementById("jogador").innerHTML=capitalizeFirstLetter(targetObject.id)
+        document.getElementById("maquina").innerHTML=capitalizeFirstLetter(personagens[escolhaMaquina].id)
+        document.querySelector("#resultado span").innerHTML = "Jogador "+String(pontuacaojogador)+"x"+String(pontuacaoMaquina)+" Máquina"
     })  
 })
 
